085a21c87ec7e050e468d2522e07023a1c95fb85
winsert2017 <winsert@163.com>
2017-03-20 21:05:59 +0800

    print data

--- post.py
+++ post.py
@@ -3,6 +3,7 @@ import urllib2
 
 values ={"uesername":"", "password":""}
 data = urllib.urlencode(values)
+print data
 url = "https://passport.csdn.net/"
 request = urllib2.Request(url,data)
 response = urllib2.urlopen(request)
