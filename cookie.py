381b335472f819a02df84e2fc3f1b6d7a1b7fcf3
winsert2017 <winsert@163.com>
2017-03-22 11:15:17 +0800

    + save cookie to file.

--- cookie.py
+++ cookie.py
@@ -3,7 +3,9 @@
 import urllib2
 import cookielib
 
-cookie = cookielib.CookieJar() #声明一个CookieJar对象实例来保存cookie
+filename = 'cookie.txt' #设置保存cookie的文件
+
+cookie = cookielib.MozillaCookieJar(filename)  #声明一个MozillaCookieJar对象实例来保存cookie，之后写入文件
 
 handler = urllib2.HTTPCookieProcessor(cookie) #利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
 
@@ -11,6 +13,4 @@ opener = urllib2.build_opener(handler) #通过handler来构建opener
 
 response = opener.open('http://www.baidu.com')
 
-for item in cookie:
-    print 'Name = ' + item.name
-    print 'Value = ' + item.value
+cookie.save(ignore_discard = True, ignore_expires = True)
