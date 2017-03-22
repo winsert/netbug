3ac4e04793c3b21380939d2a60a1bc6acf9de98c
winsert2017 <winsert@163.com>
2017-03-22 09:54:42 +0800

    +hasattr

--- try.py
+++ try.py
@@ -4,10 +4,9 @@ req = urllib2.Request('http://blog.csdn.net/cqcre')
 
 try:
     urllib2.urlopen(req)
-except urllib2.HTTPError, e:
-    print e.code
 except urllib2.URLError, e:
-    print e.reason
+    if hasattr(e, "reason"):
+        print e.reason
 else:
     print "OK"
 
