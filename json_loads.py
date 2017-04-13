5891fecf6a4b8e6ad15fe395933bbb0b6a85653e
winsert2017 <winsert@163.com>
2017-04-13 15:24:51 +0800

    实现对*.json文件的读取

---
+++ json_loads.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+## 对*.json文件进行读取验证
+
+import json
+
+filename = raw_input("请输入文件名(*.json):")
+
+jf = open(filename, 'r')
+
+data = json.loads(jf.read())
+
+print data
+print
+
+for x, y in data.items():
+    print x, y
+
+jf.close()
